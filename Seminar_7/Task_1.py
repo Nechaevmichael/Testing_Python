2ca650e54638d7857a4814043f0f7a12de7f0f0a
NechaevMichael <nechaevmichael@mail.ru>
2023-02-19 11:43:27 +0500

    correct_1 Seminar_7

--- Seminar_7/Task_1.py
+++ Seminar_7/Task_1.py
@@ -14,7 +14,14 @@
 Полученные строки записать в новый файл. Файл должен находится в поддиректории rezults.
 Данные необходимо записать в два разных файла:
 В первый - в виде Иванов И.И.
-Во второй - в виде Иванов-И-И'''
+Во второй - в виде Иванов-И-И
+
+3. Имея список [['Иванов', 'Иван', 'Иванович'], ['Петров', 'Петр', 'Петрович'], ['Соколов', 'Илья', 'Григорьевич']]
+преобразовать его в списки вида
+['Иванов-И-И'...]
+['Иванов И. И.'...]
+с использованием Comprehension; Comprehension + функция; Comprehension + lambda; map
+Усложение - дополнить обработку списка условием: Выбираем только те элементы, в которых первая буква П'''
 
 from pathlib import Path
 MAIN_DIR = Path(__file__).parent
@@ -41,4 +48,26 @@ def file_write(file_name: str, file_name_1: str) -> None:
                 file_write2.write(template2)
 # file_read(r'Seminar_7\data\name.txt')
 # file_read(MAIN_DIR / 'data' / 'name.txt')
-file_write(MAIN_DIR / 'result' / 'res.txt', MAIN_DIR / 'result' / 'res1.txt')
+# file_write(MAIN_DIR / 'result' / 'res.txt', MAIN_DIR / 'result' / 'res1.txt')
+
+
+lst = [['Иванов', 'Иван', 'Иванович'], ['Петров', 'Петр', 'Петрович'], ['Соколов', 'Илья', 'Григорьевич']]
+
+# lst2 = [f'{man[0]}-{man[1][0]}-{man[2][0]}' for man in lst]
+# lst2 = [f'{surname}-{name[0]}-{parent[0]}' for surname, name, parent in lst]
+# print(lst2)
+
+# lst3 = [f'{man[0]} {man[1][0]}. {man[2][0]}.' for man in lst]
+# lst4 = [f'{surname} {name[0]}. {parent[0]}.'for surname, name, parent in lst]
+# print(lst3)
+# print(lst4)
+
+def template(surname, name, parent):
+    return f'{surname} {name[0]}. {parent[0]}'
+
+lst4 = [template(surname, name, parent) for surname, name, parent in lst]
+print(lst4)
+
+lst5 = [(lambda surname, name, parent: f'{surname} {name[0]}. {parent[0]}')(surname, name, parent) for surname, name, parent in lst]
+print(lst5)
+
